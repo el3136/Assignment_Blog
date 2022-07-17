43c0e0c11f7e699f986b5a045876b999465889cf
Eric Lin <el3136@nyu.edu>
2022-07-17 19:38:15 -0400

    Modify configuration

--- src/firebase-config.js
+++ src/firebase-config.js
@@ -6,13 +6,13 @@ import { getAuth, GoogleAuthProvider } from 'firebase/auth';
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyBfk22UY-T3TCyudjrG0HEsjeI3y1RrQRc",
-  authDomain: "tech-incubator-5d61e.firebaseapp.com",
-  projectId: "tech-incubator-5d61e",
-  storageBucket: "tech-incubator-5d61e.appspot.com",
-  messagingSenderId: "898924687434",
-  appId: "1:898924687434:web:cda670f5270a4ce212b6b3",
-  measurementId: "G-8N1MRVE4G9"
+  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_FIREBASE_APP_ID,
+  measurementId: process.env.REACT_APP_FIREBASE_MEASUREMENT_ID
 };
 
 // Initialize Firebase
